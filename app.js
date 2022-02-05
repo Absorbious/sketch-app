6c1f4f2254f00b1015f47d48158fcbf87733e77c
Adnan Jusic <adnanjusic98@gmail.com>
2022-02-05 20:18:52 +0100

    Added event listener for hovering over boxes

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const container = document.querySelector('.container');
 let input = parseInt(prompt('Which grid dimensions?'))
 let boxes = input * input;
-let divs = container.childNodes;
+const divs = container.childNodes;
 let containerWidth = container.clientWidth;
 let boxWidth = containerWidth / input;
 let boxWidthStr = boxWidth.toString();
 
+
+
 for(let i = 1; i <= boxes; i++){
     container.appendChild(document.createElement('div')); 
-}
+};
 
 for(div of divs){
      div.style.width = `${boxWidthStr}px`;
-}
+     div.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'black';
+    })
+};
+
 
 
