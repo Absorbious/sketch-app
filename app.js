03d609d20b28e3c822db3bac73ff251a2d21686b
Adnan Jusic <adnanjusic98@gmail.com>
2022-02-08 20:10:18 +0100

    Dynamically changing slider text, must yet apply value to grid size

--- app.js
+++ app.js
@@ -4,17 +4,31 @@ const rainbowBtn = document.querySelector(".rainbow");
 const resetBtn = document.querySelector(".reset");
 const blackBtn = document.querySelector(".black");
 const divs = container.childNodes;
-let input = 0;
+const slider = document.querySelector(".slider");
+const sliderVal = document.querySelector(".sliderVal");
+
+let input = parseInt(slider.value);
+let blackRgb = 0;
 let boxes = input * input;
 let containerWidth = container.clientWidth;
 let boxWidth = (containerWidth / input).toString();
-let num = () => {return Math.floor(Math.random() * 255) + 1;};
-let blackRgb = 0;
 let newInput;
 let newBoxes;
 let newBoxWidth;
 let newBoxWidthStr;
 
+sliderVal.innerHTML = `${slider.value} x ${slider.value}`
+slider.addEventListener('change', function(){
+  sliderVal.innerHTML = `${slider.value} x ${slider.value}`
+  input = slider.value;
+  boxes;
+  boxWidth;
+  makeBoxes(boxes, boxWidth);
+})
+
+let num = () => {
+  return Math.floor(Math.random() * 255) + 1;
+};
 
 window.addEventListener("load", firstMake);
 resetBtn.addEventListener("click", newGrid);
@@ -25,11 +39,11 @@ function firstMake() {
   while (container.firstChild) {
     container.removeChild(container.lastChild);
   }
-  while (input < 2 || input > 64) {
-    input = parseInt(prompt("Which grid dimensions? (Maximum 64)"));
-    boxes = input * input;
-    boxWidth = (containerWidth / input).toString();
-  }
+  // while (input < 2 || input > 64) {
+  //   input = parseInt(prompt("Which grid dimensions? (Maximum 64)"));
+  //   boxes = input * input;
+  //   boxWidth = (containerWidth / input).toString();
+  // }
   makeBoxes(boxes, boxWidth);
 }
 
@@ -48,9 +62,11 @@ function newGrid() {
 function makeBoxes(boxCount, boxSize) {
   while (container.firstChild) {
     container.removeChild(container.lastChild);
+    console.log('removed divs')
   }
   for (let i = 1; i <= boxCount; i++) {
     container.appendChild(document.createElement("div"));
+    console.log('created divs')
   }
 
   for (div of divs) {
@@ -58,10 +74,11 @@ function makeBoxes(boxCount, boxSize) {
     div.addEventListener("mouseover", (e) => {
       e.target.style.backgroundColor = `rgb(${blackRgb}, ${blackRgb}, ${blackRgb})`;
     });
+    console.log('for every div')
   }
 }
 
-function rainbow(){
+function rainbow() {
   for (div of divs) {
     div.addEventListener("mouseover", (e) => {
       e.target.style.backgroundColor = `rgb(${num()}, ${num()}, ${num()})`;
@@ -69,7 +86,7 @@ function rainbow(){
   }
 }
 
-function black(){
+function black() {
   for (div of divs) {
     div.addEventListener("mouseover", (e) => {
       e.target.style.backgroundColor = `rgb(${blackRgb}, ${blackRgb}, ${blackRgb})`;
@@ -77,3 +94,14 @@ function black(){
   }
 }
 
+function lighten() {
+  for (div of divs) {
+    div.addEventListener("mouseover", (e) => {
+      console.log(
+        (e.target.style.backgroundColor = `rgb(${blackRgb + 50}, ${
+          blackRgb + 10
+        }, ${blackRgb + 50})`)
+      );
+    });
+  }
+}
